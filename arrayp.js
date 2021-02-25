23d79f8a34b2c018710042acf01aa5bf4be63651
ganeshappikatla <ganeshjustgrow@gmail.com>
2021-02-25 12:27:07 +0530

    added files

--- arrayp.js
+++
@@ -1,9 +0,0 @@
-var arr=[200,599,2456,"hrlo","hfhf"];
-
-console.log(arr);
-console.log(arr[5])
-
-arr.push("nallu")
-console.log(arr)
-arr.unshift("hehe")
-console.log(arr)
---
+++ tasks/valueandreference.js
@@ -0,0 +1,7 @@
+let original=10;
+function updateoriginal(originalval){
+    originalval+=10;
+    console.log(originalval);
+}
+    updateoriginal(original);
+    console.log(original);
